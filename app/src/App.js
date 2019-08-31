6acae6ab4879449f4cece8c273de49dd08b71dfa
Juan Orero <juaoregon@gmail.com>
2019-08-31 19:01:46 +0200

    Ejercicio 2 - Lista de jugadores con sus nombres

--- app/src/App.js
+++ app/src/App.js
@@ -1,47 +1,47 @@
-import reactSvg from './react.svg'
+import React, {PureComponent} from 'react'
 import './App.css'
+import reactSvg from './react.svg'
 
-import React, { PureComponent } from 'react'
 const domain = 'http://localhost:3001'
 
 class App extends PureComponent {
   state = {
-    teams: []
+    players: []
   }
 
   componentDidMount() {
-    fetch(`${domain}/teams`)
-      .then(response => {
-        return response.json();
-      })
-      .then(teams => {
-        this.setState({ teams })
-      });
+    fetch(`${domain}/players`).then(response => {
+      return response.json();
+    }).then(players => {
+      this.setState({players})
+    });
   }
 
   render() {
-    const { teams } = this.state
+    const {players} = this.state
 
     return <div className="App">
       <header className="App-heading App-flex">
         <h2>Bienvenido a la prueba de los equipos</h2>
       </header>
       <div className="App-teams App-flex">
-        {/* 
+        {/*
           TODO ejercicio 2
-          Debes obtener los players en lugar de los equipos y pintar su nombre. 
+          Debes obtener los players en lugar de los equipos y pintar su nombre.
           Borra todo el código que no sea necesario. Solo debe existir un título: Los jugadores
-          y una lista con sus nombres. 
+          y una lista con sus nombres.
           ** Los comentarios de los ejercicios no los borres.
-        */}
-        <h3>Los equipos:</h3>
+        */
+        }
+        <h3>Los jugadores:</h3>
         <ul>
-          {/* 
+          {/*
             TODO ejercicio 3
             Vamos a pasar a darle diseño. Crea el diseño propuesto en el readme con los requerimientos que se necesite.
             Guiate por las imágenes.
-           */}
-          {teams.map(team => <li key={team.id}>{team.name}</li>)}
+           */
+          }
+          {players.map(player => <li key={player.id}>{player.name}</li>)}
         </ul>
       </div>
       <div className="App-instructions App-flex">
