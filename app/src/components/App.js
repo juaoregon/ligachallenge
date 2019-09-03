ab89b5c8133c4e7ceb103c1673f92347213021e0
Juan Orero <juaoregon@gmail.com>
2019-09-03 03:05:27 +0200

    Ejercicio 8 - usar datos de localStorage en caso de tenerlos

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -9,7 +9,11 @@ const domain = 'http://localhost:3001'
 class App extends PureComponent {
   constructor(props) {
     super(props);
-    this.state = { players: [], teams: [], pichichis: [] }
+    this.state = {
+      players: JSON.parse(localStorage.getItem('players')) || [],
+      teams: JSON.parse(localStorage.getItem('teams')) || [],
+      pichichis: JSON.parse(localStorage.getItem('pichichis')) || []
+    }
     this.sortTable = this.sortPichichis.bind(this)
   }
 
@@ -18,12 +22,14 @@ class App extends PureComponent {
       return response.json();
     }).then(players => {
       this.setState({players})
+      localStorage.setItem('players', JSON.stringify(players));
     });
 
     fetch(`${domain}/teams`).then(response => {
       return response.json();
     }).then(teams => {
       this.setState({teams})
+      localStorage.setItem('teams', JSON.stringify(teams));
     });
 
     fetch(`${domain}/pichichis`).then(response => {
@@ -33,7 +39,8 @@ class App extends PureComponent {
       pichichis.forEach((pichichi, i) => {
         pichichisList.push(Object.assign({}, pichichi, this.state.players[i]));
       })
-      this.setState({pichichis: pichichisList});
+      this.setState({pichichis: pichichisList})
+      localStorage.setItem('pichichis', JSON.stringify(pichichisList));
     });
   }
 
@@ -67,35 +74,33 @@ class App extends PureComponent {
   }
 
   render() {
-    return <div className="App">
-      <header className="App-heading">
-        <a onClick={this.onClickPichichis}>Pichichis</a>
-      </header>
-      <div className="App-players App-flex">
-        {/*
-          TODO ejercicio 2
-          Debes obtener los players en lugar de los equipos y pintar su nombre.
-          Borra todo el código que no sea necesario. Solo debe existir un título: Los jugadores
-          y una lista con sus nombres.
-          ** Los comentarios de los ejercicios no los borres.
-        */
-        }
-        {/*
-          TODO ejercicio 3
-          Vamos a pasar a darle diseño. Crea el diseño propuesto en el readme con los requerimientos que se necesite.
-          Guiate por las imágenes.
-         */
-        }
-        {this.state.players.map(player =>
-          <Player key={player.id} player={player} team={this.state.teams.find(team => {return player.teamId === team.id})}
-        />)}
+    return (
+      <div className="App">
+        <header className="App-heading">
+          <a onClick={this.onClickPichichis}>Pichichis</a>
+        </header>
+        <div className="App-players App-flex">
+          {/*
+            TODO ejercicio 2
+            Debes obtener los players en lugar de los equipos y pintar su nombre.
+            Borra todo el código que no sea necesario. Solo debe existir un título: Los jugadores
+            y una lista con sus nombres.
+            ** Los comentarios de los ejercicios no los borres.
+          */
+          }
+          {/*
+            TODO ejercicio 3
+            Vamos a pasar a darle diseño. Crea el diseño propuesto en el readme con los requerimientos que se necesite.
+            Guiate por las imágenes.
+           */
+          }
+          {this.state.players.map(player =>
+            <Player key={player.id} player={player} team={this.state.teams.find(team => {return player.teamId === team.id})}
+          />)}
+        </div>
+        <Pichichis onSort={this.sortPichichis} pichichis={this.state.pichichis} />
       </div>
-      <div className="App-instructions App-flex">
-        <img className="App-logo" src={reactSvg}/>
-        <p>Edit <code>src/App.js</code> and save to hot reload your changes.</p>
-      </div>
-      <Pichichis onSort={this.sortPichichis} pichichis={this.state.pichichis} />
-    </div>
+    );
   }
 }
 
